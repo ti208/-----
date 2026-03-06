179789650ae9be25955516fb55c20c4ee2253348
tico <0970226763tico@gmail.com>
2026-03-06 16:56:47 +0800

    first commit

---
+++ celsius_to_fahrenheit.c
@@ -0,0 +1,32 @@
+/**
+ * celsius_to_fahrenheit.c
+ *
+ * Convert Celsius to Fahrenheit
+ *
+ * ============= 執行畫面範例 =============
+ * Please enter Celsius: 25
+ * The Fahrenheit is: 77.000000
+ * =======================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float celsius = 0.0, fahrenheit = 0.0;
+    printf("Please enter Celsius:");
+    scanf("%f", &celsius);
+    // ===================================
+    // Input
+    // ===================================
+    // 請輸出 "Please enter Celsius: " 並讀取攝氏溫度
+
+    // ===================================
+    // Calculation
+    // ===================================
+    fahrenheit = celsius * 9 / 5 + 32;
+    printf("%f",fahrenheit);
+    return 0;
+}
+    // ================================
